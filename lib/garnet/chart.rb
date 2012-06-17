918c97dfa71adfdbd33bb30f9819a3741dca919f
Lee Dohm <lee@liftedstudios.com>
2012-06-16 19:08:30 -0700

    Cleaning up documentation.

--- lib/garnet/chart.rb
+++ lib/garnet/chart.rb
@@ -13,14 +13,15 @@ module Garnet
     # Width of the generated image.
     attr_reader :width
 
-    # Initialize a new instance of the +Garnet::Chart+ class.
+    # Initialize a new instance of the +Chart+ class.
     # 
     # @param width Width of the image to generate.
     # @param height Height of the image to generate.
+    # @raise [ArgumentError] When width or height are negative.
     def initialize(width, height)
       raise ArgumentError, "Width cannot be negative" if width < 0
       raise ArgumentError, "Height cannot be negative" if height < 0
-      
+
       @width = width
       @height = height
     end
