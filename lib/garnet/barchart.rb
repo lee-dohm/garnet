733cdff265497189d2c67e6bb10e114fedff2a06
Lee Dohm <lee@liftedstudios.com>
2012-06-20 17:45:34 -0700

    Chart is now scaled properly.

--- lib/garnet/barchart.rb
+++ lib/garnet/barchart.rb
@@ -7,13 +7,26 @@
 module Garnet
   # Defines the standard bar chart type.
   class BarChart
+    # Colors:
+    # 89 154 211
+    # 241 89 95
+    # 121 195 106
+    # 249 166 90
+    # 158 102 171
+    # 205 112 88
+    # 215 127 179
+    # 114 114 114
+
     # Renders the chart as a standard bar chart.
     # 
     # @param [Builder::XmlMarkup] builder Builder object to use to emit XML.
     # @param [Chart] chart Object describing the chart.
     def self.render(builder, chart)
-      builder.g do |b|
-        max = chart.data.max
+      max = chart.data.max
+      scale_x = chart.display_rect[2] / (chart.data.count * 5 + 1 + 4 + 1)
+      scale_y = chart.display_rect[3] / max
+
+      builder.g(:transform => "scale(#{scale_x}, #{scale_y})") do |b|
         chart.data.each_with_index do |datum, index|
           b.rect(:x => (index * 5 + 1), :y => (max - datum), :width => 4, :height => datum.to_s)
         end
--- spec/barchart_spec.rb
+++ spec/barchart_spec.rb
@@ -19,10 +19,15 @@ include Test::Helpers
 
 describe BarChart do
   before do
+    @data = [10, 20, 30]
+    @display_rect = [0, 0, 1200, 900]
+
     @mock = MiniTest::Mock.new
-    @mock.expect(:data, [10, 20, 30])
-    @mock.expect(:data, [10, 20, 30])
-    @mock.expect(:display_rect, [0, 0, 1200, 900])
+    @mock.expect(:data, @data)
+    @mock.expect(:data, @data)
+    @mock.expect(:data, @data)
+    @mock.expect(:display_rect, @display_rect)
+    @mock.expect(:display_rect, @display_rect)
   end
 
   it 'will render the chart within a group' do
@@ -34,25 +39,23 @@ describe BarChart do
   it 'will render each bar as four units wide' do
     xml = BarChart.render(Builder::XmlMarkup.new(:indent => 2), @mock)
 
-    xml.must_have_count_elements "g/rect", 3
+    xml.must_have_count_elements "g/rect", @data.count
     xml.must_have_attribute_on_element_equal "width", "g/rect", "4"
   end
 
   it 'will render each bar as its value high' do
-    data = [10, 20, 30]
-
     xml = BarChart.render(Builder::XmlMarkup.new(:indent => 2), @mock)
 
-    xml.must_have_count_elements "g/rect", 3
-    xml.must_have_attribute_on_element_equal "height", "g/rect", data
+    xml.must_have_count_elements "g/rect", @data.count
+    xml.must_have_attribute_on_element_equal "height", "g/rect", @data
   end
 
   it 'will render each bar at an x-position of five times its index plus one' do
-    data = [1, 6, 11]
+    data = 0.upto(2).map { |i| 5 * i + 1 }
 
     xml = BarChart.render(Builder::XmlMarkup.new(:indent => 2), @mock)
 
-    xml.must_have_count_elements "g/rect", 3
+    xml.must_have_count_elements "g/rect", @data.count
     xml.must_have_attribute_on_element_equal "x", "g/rect", data
   end
 
@@ -61,7 +64,17 @@ describe BarChart do
 
     xml = BarChart.render(Builder::XmlMarkup.new(:indent => 2), @mock)
 
-    xml.must_have_count_elements "g/rect", 3
+    xml.must_have_count_elements "g/rect", @data.count
     xml.must_have_attribute_on_element_equal "y", "g/rect", data
+  end
+
+  it 'will add a transform attribute to the group to scale to the display rect' do
+    # Width divided by x-coord of the last bar plus width of the last bar plus one for the margin at the edge.
+    scale_x = @display_rect[2] / (@data.count * 5 + 1 + 4 + 1)
+    scale_y = @display_rect[3] / @data.max
+
+    xml = BarChart.render(Builder::XmlMarkup.new(:indent => 2), @mock)
+
+    xml.must_have_attribute_on_element_equal "transform", "g", "scale(#{scale_x}, #{scale_y})"
   end
 end
