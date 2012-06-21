a1b209a894696d3daa3c16d9a2ac25d68b1f1ca5
Lee Dohm <lee@liftedstudios.com>
2012-06-20 17:50:08 -0700

    Refactoring.

--- lib/garnet/barchart.rb
+++ lib/garnet/barchart.rb
@@ -17,18 +17,25 @@ module Garnet
     # 215 127 179
     # 114 114 114
 
+    OUTSIDE_BAR_MARGIN = 1
+    BETWEEN_BAR_MARGIN = 1
+    BAR_WIDTH = 4
+
     # Renders the chart as a standard bar chart.
     # 
     # @param [Builder::XmlMarkup] builder Builder object to use to emit XML.
     # @param [Chart] chart Object describing the chart.
     def self.render(builder, chart)
       max = chart.data.max
-      scale_x = chart.display_rect[2] / (chart.data.count * 5 + 1 + 4 + 1)
+      scale_x = chart.display_rect[2] / (chart.data.count * (BAR_WIDTH + BETWEEN_BAR_MARGIN) + BAR_WIDTH + 2 * OUTSIDE_BAR_MARGIN)
       scale_y = chart.display_rect[3] / max
 
       builder.g(:transform => "scale(#{scale_x}, #{scale_y})") do |b|
         chart.data.each_with_index do |datum, index|
-          b.rect(:x => (index * 5 + 1), :y => (max - datum), :width => 4, :height => datum.to_s)
+          b.rect(:x => (index * (BAR_WIDTH + BETWEEN_BAR_MARGIN) + OUTSIDE_BAR_MARGIN), 
+                 :y => (max - datum), 
+                 :width => BAR_WIDTH, 
+                 :height => datum.to_s)
         end
       end
     end
